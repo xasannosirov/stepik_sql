7658d8cfae93fcf71ac9d3b48cc3063e64d6e5ed
xasannosirov <xasannosirov094@gmail.com>
2024-01-12 19:12:31 +0500

    added 1_2 step 7 problem

--- modul_1/1_2.sql
+++ modul_1/1_2.sql
@@ -13,3 +13,7 @@ SELECT title AS Название, author AS Автор FROM book;
 -- step 5
 
 SELECT title, amount, amount*1.65 AS pack FROM book;
+
+-- step 6
+
+SELECT title, author, amount, ROUND(price-price*0.3, 2) AS new_price FROM book;
