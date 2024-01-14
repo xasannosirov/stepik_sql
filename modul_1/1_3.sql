0742e5d2c626b3c93c34b053f0b7eec17072c720
xasannosirov <xasannosirov094@gmail.com>
2024-01-14 16:39:18 +0500

    changed 1_3.sql file

--- modul_1/1_3.sql
+++ modul_1/1_3.sql
@@ -6,3 +6,26 @@ FROM
     book 
 ORDER BY 
     amount;
+
+-- step 3
+
+SELECT 
+    author AS Автор, 
+    COUNT(author) AS Различных_книг, 
+    SUM(amount) AS Количество_экземпляров
+FROM 
+    book
+GROUP BY 
+    author;
+
+-- step 4
+
+SELECT
+    author,
+    MIN(price) AS Минимальная_цена,
+    MAX(price) AS Максимальная_цена,
+    AVG(price) AS Средняя_цена
+FROM
+    book
+GROUP BY
+    author;
